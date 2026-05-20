af8db7159db619b871fef1024d8b6cae82888664
Bárbara Rosa <32175889+thebabirosa@users.noreply.github.com>
2026-05-20 10:30:52 -0300

    o

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ export default function App() {
           <div className="relative w-full bg-black aspect-video">
             <iframe
               id="vslVideo"
-              src="https://player.vimeo.com/video/1193649372?autoplay=1&title=0&byline=0&portrait=0"
+              src="https://player.vimeo.com/video/1193649372?autoplay=1&title=0&byline=0&portrait=0&controls=0&dnt=1"
               allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
               allowFullScreen
               className="w-full h-full border-none"
@@ -77,16 +77,7 @@ export default function App() {
                   </div>
                 </div>
               </motion.div>
-            ) : (
-              import.meta.env.DEV && (
-                <div 
-                  className="text-xs text-white/30 cursor-pointer select-none p-4" 
-                  onClick={() => setShowContent(true)}
-                >
-                  [Modo Dev: Clique aqui para mostrar o conteúdo e o botão]
-                </div>
-              )
-            )}
+            ) : null}
           </div>
         </div>
       </div>
